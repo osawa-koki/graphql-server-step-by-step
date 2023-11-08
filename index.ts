669928cf26e6cbf8220a55d065a4d23ba88bffd3
osawa-koki <koki@osawa.dev>
2023-11-09 00:20:28 +0900

    yarn lint --fix

--- index.ts
+++ index.ts
@@ -1,41 +1,41 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { loadSchemaSync } from '@graphql-tools/load';
-import { addResolversToSchema } from '@graphql-tools/schema';
-import { Book, Resolvers } from './src/generated/graphql';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { addResolversToSchema } from '@graphql-tools/schema'
+import { type Book, type Resolvers } from './src/generated/graphql';
 
 (async () => {
   const schema = loadSchemaSync('./schemas/**/*.graphql', {
-    loaders: [new GraphQLFileLoader()],
-  });
+    loaders: [new GraphQLFileLoader()]
+  })
 
   const books: Book[] = [
     {
       title: 'The Awakening',
-      author: 'Kate Chopin',
+      author: 'Kate Chopin'
     },
     {
       title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+      author: 'Paul Auster'
+    }
+  ]
 
   const resolvers: Resolvers = {
     Query: {
-      books: (): Book[] => books,
-    },
-  };
+      books: (): Book[] => books
+    }
+  }
 
-  const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
-  const server = new ApolloServer({ schema: schemaWithResolvers });
+  const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
+  const server = new ApolloServer({ schema: schemaWithResolvers })
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
+    listen: { port: 4000 }
+  })
 
-  console.log(`🚀  Server ready at: ${url}`);
+  console.log(`🚀  Server ready at: ${url}`)
 })()
-  .then(() => console.log('Server started...'))
-  .catch((e) => console.error(e));
+  .then(() => { console.log('Server started...') })
+  .catch((e) => { console.error(e) })
